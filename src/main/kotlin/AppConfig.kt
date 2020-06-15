a2895f5849458e33f14fd9a16f8f60c703744716
Vinayagasundar <vinayagasundar93@gmail.com>
2020-06-15 19:47:35 +0800

    (chore) Updated the gradle plugin

--- src/main/kotlin/AppConfig.kt
+++ src/main/kotlin/AppConfig.kt
@@ -10,7 +10,7 @@ object AppConfig {
 
 
     object Plugins {
-        const val android = "com.android.tools.build:gradle:4.0.0-beta05"
+        const val android = "com.android.tools.build:gradle:4.1.0-beta01"
         const val kotlin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion"
         const val googleService = "com.google.gms:google-services:4.3.2"
     }
